d32a21382c7e01a3ca1db9af7d32ca601535236b
Sergio Sepulveda <contacto@penti2s.com>
2022-05-11 13:39:01 -0400

    Exportacion de clases Animales

---
+++ assets/js/clases/Animales.js
@@ -0,0 +1,48 @@
+import Animal from './Animal'
+
+class Leon extends Animal{
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+    Rugir(){
+        console.log('Rugir')
+    }
+}
+
+class Lobo extends Animal{
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+    Aullar(){
+        console.log('Aullar')
+    }
+}
+
+class Oso extends Animal{
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+    Gruñir(){
+        console.log('Gruñir')
+    }
+}
+
+class Serpiente extends Animal{
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+    Sisear(){
+        console.log('Sisear')
+    }
+}
+
+class Aguila extends Animal{
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+    Chillar(){
+        console.log('Chillar')
+    }
+}
+
+export { Leon, Lobo, Oso, Serpiente, Aguila }
