8cd74ec548d99d4353fe45e373e5c266c237fdbb
Sergio Sepulveda <contacto@penti2s.com>
2022-05-11 22:11:27 -0400

    Creacion de Clases animales con los datos del formulario

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 import { Leon, Lobo, Oso, Serpiente, Aguila } from '../js/clases/Animales.js'
 
 
-const animales = []
+const animalesTabla = []
 
 const leon1 = new Leon('Leon', 5, 'img/leon.png', 'Gruñir', 'Russssgir')
 
@@ -10,30 +10,52 @@ const leon1 = new Leon('Leon', 5, 'img/leon.png', 'Gruñir', 'Russssgir')
 
 let callAPi = ( () =>{
     const url_personajes = 'animales.json';
-    const getData = async () => {
-        const res = await fetch(url_personajes);
-        const data = await res.json();
-        return data
-    } 
-    return {getData}
+        try {
+            const getData = async () => {
+                const res = await fetch(url_personajes);
+                const data = await res.json();
+                return data
+            }
+            return {getData}
+        } catch (error) {
+            console.log(error)
+        }
 } )() 
+const { animales } =  await callAPi.getData()
 
+// Manipulacion del formulario a la hora de hacer click en el boton
 
-document.querySelector('#btnRegistrar').addEventListener('click', async () => {
+document.querySelector('#btnRegistrar').addEventListener('click', () => {
     const nombre = document.querySelector('#animal').value;
     const edad = document.querySelector('#edad').value;
     const comentarios = document.querySelector('#comentarios').value;
-    // console.log(templateImg)
-    // previewImg.innerHTML = templateImg
-    //previewImg.style.backgroundImage = animales.find( a => a.nombre === nombre ).imagen
+    const previewImg = document.querySelector('#preview');
+    const imgSrc = previewImg.style.backgroundImage.slice(5, -2);
+    let sonido = animales.find( a => a.name === nombre).sonido
+    console.log(sonido)
+    if(nombre !== '' && edad !== '' && comentarios !== '' && imgSrc !== ''){
+        if(nombre === 'Leon'){
+            animalesTabla.push(new Leon(nombre, edad, imgSrc, comentarios, `/assets/sounds/${sonido}`))
+        }else if(nombre === 'Lobo'){
+            animalesTabla.push(new Lobo(nombre, edad, imgSrc, comentarios, `/assets/sounds/${sonido}`))
+        }else if(nombre === 'Oso'){
+            animalesTabla.push(new Oso(nombre, edad, imgSrc, comentarios, `/assets/sounds/${sonido}`))
+        }else if(nombre === 'Serpiente'){
+            animalesTabla.push(new Serpiente(nombre, edad, imgSrc, comentarios, `/assets/sounds/${sonido}`))
+        }else if (nombre === 'Aguila'){
+            animalesTabla.push(new Aguila(nombre, edad, imgSrc, comentarios, `/assets/sounds/${sonido}`))
+        }
+    }else{
+        alert('Todos los campos son obligatorios')
+    }
+    console.log(animalesTabla)
 })
 
+
+// Evento que cambia la Preview de la imagen
 document.querySelector('#animal').addEventListener('change', async (e) => {
     const nombre = e.target.value;
     const previewImg = document.querySelector('#preview');
-    const { animales } =  await callAPi.getData()
     let img = animales.find( a => a.name === nombre).imagen
     previewImg.style.backgroundImage = `url(/assets/imgs/${img})`
-
-
 })
